945271811ba1307e322f8094ad9d79b047c2f804
chenxz <563859095@qq.com>
2018-12-22 21:56:35 +0800

    commit

---
+++ module_me/src/main/java/com/cxz/module/me/MeMainActivity.kt
@@ -0,0 +1,14 @@
+package com.cxz.module.me
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
+
+@Route(path = "/me/main")
+class MeMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_me_main)
+    }
+}
---
+++ module_me/src/main/java/debug/MeApplication.kt
@@ -0,0 +1,16 @@
+package debug
+
+import com.cxz.kotlin.baselibs.app.BaseApp
+
+/**
+ * @author chenxz
+ * @date 2018/12/22
+ * @desc
+ */
+class MeApplication : BaseApp() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
