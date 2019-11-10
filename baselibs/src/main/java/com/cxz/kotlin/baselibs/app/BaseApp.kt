88b1cf0dbd8355213fb86458e84ee38db07314ce
chenxz <563859095@qq.com>
2019-11-10 14:17:20 +0800

    fix

--- baselibs/src/main/java/com/cxz/kotlin/baselibs/app/BaseApp.kt
+++ baselibs/src/main/java/com/cxz/kotlin/baselibs/app/BaseApp.kt
@@ -1,15 +1,6 @@
 package com.cxz.kotlin.baselibs.app
 
-import android.app.Activity
 import android.app.Application
-import android.content.Context
-import android.os.Bundle
-import android.support.multidex.MultiDex
-import android.util.Log
-import com.alibaba.android.arouter.launcher.ARouter
-import com.cxz.kotlin.baselibs.BuildConfig
-import com.squareup.leakcanary.LeakCanary
-import com.squareup.leakcanary.RefWatcher
 import kotlin.properties.Delegates
 
 /**
@@ -19,77 +10,14 @@ import kotlin.properties.Delegates
  */
 open class BaseApp : Application() {
 
-    private var refWatcher: RefWatcher? = null
-
     companion object {
-
-        @JvmField
-        val TAG = "BaseApp"
-
-        var instance: Context by Delegates.notNull()
+        var instance: Application by Delegates.notNull()
             private set
-
-        fun getRefWatcher(context: Context): RefWatcher? {
-            val app = context.applicationContext as BaseApp
-            return app.refWatcher
-        }
-
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
-        refWatcher = setupLeakCanary()
-        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks)
-        initRouter()
     }
 
-    private fun initRouter() {
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog()
-            ARouter.openDebug()
-        }
-        ARouter.init(this)
-    }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        MultiDex.install(this)
-    }
-
-    private fun setupLeakCanary(): RefWatcher {
-        return if (LeakCanary.isInAnalyzerProcess(this)) {
-            RefWatcher.DISABLED
-        } else LeakCanary.install(this)
-    }
-
-    private val mActivityLifecycleCallbacks = object : Application.ActivityLifecycleCallbacks {
-        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-            Log.d(TAG, "onCreated: " + activity.componentName.className)
-        }
-
-        override fun onActivityStarted(activity: Activity) {
-            Log.d(TAG, "onStart: " + activity.componentName.className)
-        }
-
-        override fun onActivityResumed(activity: Activity) {
-
-        }
-
-        override fun onActivityPaused(activity: Activity) {
-
-        }
-
-        override fun onActivityStopped(activity: Activity) {
-
-        }
-
-        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-
-        }
-
-        override fun onActivityDestroyed(activity: Activity) {
-            Log.d(TAG, "onDestroy: " + activity.componentName.className)
-        }
-    }
 }
