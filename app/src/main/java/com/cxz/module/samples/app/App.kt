73b86b3fa55eb8beeb33f5f4c16decf6f74ed2f8
chenxz <563859095@qq.com>
2019-11-10 14:27:46 +0800

    fix

--- app/src/main/java/com/cxz/module/samples/app/App.kt
+++ app/src/main/java/com/cxz/module/samples/app/App.kt
@@ -1,8 +1,15 @@
 package com.cxz.module.samples.app
 
+import android.app.Activity
 import android.content.Context
+import android.os.Bundle
 import android.support.multidex.MultiDex
+import com.alibaba.android.arouter.launcher.ARouter
+import com.cxz.kotlin.baselibs.BuildConfig
 import com.cxz.kotlin.baselibs.app.BaseApp
+import com.cxz.kotlin.baselibs.utils.NLog
+import com.squareup.leakcanary.LeakCanary
+import com.squareup.leakcanary.RefWatcher
 
 /**
  * @author chenxz
@@ -11,13 +18,72 @@ import com.cxz.kotlin.baselibs.app.BaseApp
  */
 class App : BaseApp() {
 
+    private val TAG = "App"
+
+    private var refWatcher: RefWatcher? = null
+
+    companion object {
+        fun getRefWatcher(context: Context): RefWatcher? {
+            val app = context.applicationContext as App
+            return app.refWatcher
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
+        initLeakCanary()
+        initRouter()
+    }
+
+    private fun initLeakCanary() {
+        refWatcher = if (LeakCanary.isInAnalyzerProcess(this))
+            RefWatcher.DISABLED
+        else LeakCanary.install(this)
+
+        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks)
+    }
+
+    private fun initRouter() {
+        if (BuildConfig.DEBUG) {
+            ARouter.openLog()
+            ARouter.openDebug()
+        }
+        ARouter.init(this)
     }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         MultiDex.install(this)
+    }
+
+    private val mActivityLifecycleCallbacks = object : ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            NLog.d(TAG, "onCreated: " + activity.componentName.className)
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+            NLog.d(TAG, "onStart: " + activity.componentName.className)
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+            NLog.d(TAG, "onResume: " + activity.componentName.className)
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+            NLog.d(TAG, "onPause: " + activity.componentName.className)
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+            NLog.d(TAG, "onStop: " + activity.componentName.className)
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            NLog.d(TAG, "onSaveInstanceState: " + activity.componentName.className)
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            NLog.d(TAG, "onDestroy: " + activity.componentName.className)
+        }
     }
 
 }
